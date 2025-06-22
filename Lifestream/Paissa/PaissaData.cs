66df5500b42faa33cf9e055a61bc2990199af537
Mashiro-chan <11561006+mashirochan@users.noreply.github.com>
2025-06-21 23:27:01 -0400

    Add Paissa table columns

--- Lifestream/Paissa/PaissaData.cs
+++ Lifestream/Paissa/PaissaData.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using Lifestream.Data;
+using Lifestream.Enums;
+using Newtonsoft.Json;
+using NightmareUI.OtterGuiWrapper.FileSystems.Generic;
 
 namespace Lifestream.Paissa;
 
@@ -64,8 +67,59 @@ public static class PaissaData
         public float? LottoPhaseUntil;
     }
 
+    public class PaissaResult
+    {
+        public PaissaStatus Status { get; set; }
+        public string FolderText { get; set; }
+    }
+
     public enum PaissaStatus
     {
         Idle, Progress, Success, Error
+    }
+
+    public class PaissaAddressBookEntry : AddressBookEntry
+    {
+        public int Size = 1;
+        public int Bids = 1;
+        public int AllowedTenants = 1;
+    }
+
+    public static PaissaAddressBookEntry ToPaissa(this AddressBookEntry entry)
+    {
+        var tuple = entry.AsTuple();
+        return new PaissaAddressBookEntry {
+            Name = tuple.Name,
+            World = tuple.World,
+            City = (ResidentialAetheryteKind)tuple.City,
+            Ward = tuple.Ward,
+            PropertyType = (PropertyType)tuple.PropertyType,
+            Plot = tuple.Plot,
+            Apartment = tuple.Apartment,
+            ApartmentSubdivision = tuple.ApartmentSubdivision,
+            AliasEnabled = tuple.AliasEnabled,
+            Alias = tuple.Alias,
+            Size = 1,
+            Bids = 1,
+            AllowedTenants = 1,
+        };
+    }
+
+    [Serializable]
+    public class PaissaAddressBookFolder : IFileSystemStorage
+    {
+        internal bool IsCopy = false;
+        public string ExportedName = "";
+        public Guid GUID { get; set; } = Guid.NewGuid();
+        public List<PaissaAddressBookEntry> Entries = [];
+        public bool IsDefault = false;
+        public SortMode SortMode = SortMode.Manual;
+
+        public bool ShouldSerializeGUID() => !IsCopy;
+        public bool ShouldSerializeIsDefault() => !IsCopy;
+        public bool ShouldSerializeExportedName() => IsCopy;
+
+        public string GetCustomName() => null;
+        public void SetCustomName(string s) { }
     }
 }
